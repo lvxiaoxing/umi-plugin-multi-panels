214567ea4cf57b52fc23eff19827cc61c83da526
xieyingixing <jusda-sh-xieyingxing@jusdascm.com>
2022-05-20 09:51:36 +0800

    🐞 fix: update default route

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ export default function (api: IApi) {
         tabsLimitWarnTitle: '提示',
         tabsLimitWarnContent:
           '您当前打开页面过多, 请关闭不使用的页面以减少卡顿!',
-        defaultPage:'/welcome'
+        defaultRoute:'/welcome',
       },
       schema(joi) {
         return joi.object({
@@ -71,6 +71,7 @@ export default function (api: IApi) {
           tabsLimitWait: joi.number(),
           tabsLimitWarnTitle: joi.string(),
           tabsLimitWarnContent: joi.string(),
+          defaultRoute:joi.string(),
         });
       },
       onChange: api.ConfigChangeType.regenerateTmpFiles,
@@ -132,6 +133,7 @@ export default function (api: IApi) {
         ),
         {
           ...api.config.panelTab,
+          defaultRoute:decodeURIComponent(api.config.panelTab.defaultRoute),
           useI18n: api.userConfig?.locale && api.config.panelTab?.autoI18n,
         },
         {},
